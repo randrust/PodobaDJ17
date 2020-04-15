530ebab9654fa2942839ae3116e569370142efbd
Andru Rakutsky <randrur@gmail.com>
2020-04-15 14:32:56 +0300

    add context_processors.py

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -82,3 +82,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PORTAL_URL = 'http://localhost:8000'
+
+from django.conf import global_settings
+
+TEMPLATE_CONTEXT_PROCESSORS = global_settings.TEMPLATE_CONTEXT_PROCESSORS + \
+                              ("django.core.context_processors.request",
+                               "studentsdb.context_processors.students_proc",)
+
+
+
